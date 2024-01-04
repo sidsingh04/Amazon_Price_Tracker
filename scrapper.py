b531a039c97f538afd9f59978ea0335229f980c9
sidsingh04 <121860966+sidsingh04@users.noreply.github.com>
2024-01-04 11:41:21 +0530

    Update scrapper.py
    updated request headers for efficient tracking

--- scrapper.py
+++ scrapper.py
@@ -8,7 +8,16 @@ from datetime import date
 # this would return dictionary if the url is valid otherwise None
 # as a extra feature we could add image url of amazon of the product in the site
 def extract_Data(url):
-    headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36' }
+    # headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36' }
+    headers = { 
+               'Accept-Encoding':'gzip, deflate, br',
+               'Accept-Language':'en-US,en;q=0.9',
+               'Referer':'https://www.amazon.in/',
+               'Sec-Ch-Ua':'"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+               'Sec-Ch-Ua-Mobile':'?0',
+               'Sec-Ch-Ua-Platform':"Windows",
+              'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'
+    }
     try:
         res=requests.get(url,headers=headers)
         soup=BeautifulSoup(res.content,'html.parser')
